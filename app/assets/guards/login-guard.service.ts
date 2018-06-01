0af518398a71d86fe84519026c67c6d9e913c724
Berfu Taluy <berfu.taluy@navara.nl>
2018-06-01 16:22:54 +0200

    enabled the guard again

--- app/assets/guards/login-guard.service.ts
+++ app/assets/guards/login-guard.service.ts
@@ -13,8 +13,8 @@ export class LoginGuard implements CanActivate {
     private navigationService: NavigationService
   ) {}
 
-  public canActivate(): boolean {
-    /*return this.isUserLoggedIn()
+  public canActivate(): Promise<boolean> {
+    return this.isUserLoggedIn()
       .then(message => {
         return true;
       })
@@ -22,11 +22,10 @@ export class LoginGuard implements CanActivate {
         console.log(error);
         this.navigationService.navigateToAndClearHistory(MenuItemName.login);
         return false;
-      });*/
-      return true;
+      });
   }
 
-  /*private isUserLoggedIn(): Promise<any> {
+  private isUserLoggedIn(): Promise<any> {
     return new Promise((promise: any, reject: any) => {
       firebase.getCurrentUser()
       .then(() => {
@@ -36,5 +35,5 @@ export class LoginGuard implements CanActivate {
         return reject('No user logged in!');
       })
     });
-  }*/
+  }
 }
