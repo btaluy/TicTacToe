19d7d041a3346083feea534a7e66764439f98a05
Berfu Taluy <berfu.taluy@navara.nl>
2018-07-09 10:07:52 +0200

    Added documentation for tech stuff.

--- app/assets/domain/navigation-item.js
+++ app/assets/domain/navigation-item.js
@@ -6,3 +6,4 @@ var NavigationItem = /** @class */ (function () {
     return NavigationItem;
 }());
 exports.NavigationItem = NavigationItem;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibmF2aWdhdGlvbi1pdGVtLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibmF2aWdhdGlvbi1pdGVtLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBRUE7SUFBQTtJQUdBLENBQUM7SUFBRCxxQkFBQztBQUFELENBQUMsQUFIRCxJQUdDO0FBSFksd0NBQWMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBNZW51SXRlbU5hbWUgfSBmcm9tICcuL21lbnUtaXRlbSc7XG5cbmV4cG9ydCBjbGFzcyBOYXZpZ2F0aW9uSXRlbSB7XG4gIHB1YmxpYyBuYW1lOiBNZW51SXRlbU5hbWU7XG4gIHB1YmxpYyBwYXRoOiBzdHJpbmc7XG59Il19
